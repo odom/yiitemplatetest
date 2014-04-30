15a7c4622c426cd15d6086e8a8db93c49b31e22e
Sopheak Se <sopheak.se@mapring.com>
2014-04-30 19:01:04 +0700

    [WIP] Include Twitter Bootstrap into project and design login form

--- myapp/protected/views/site/login.php
+++ myapp/protected/views/site/login.php
@@ -11,8 +11,6 @@ $this->breadcrumbs=array(
 
 <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
-
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'login-form',
@@ -20,9 +18,7 @@ $this->breadcrumbs=array(
 	'clientOptions'=>array(
 		'validateOnSubmit'=>true,
 	),
-)); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+)); ?>	
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'username'); ?>
@@ -33,10 +29,7 @@ $this->breadcrumbs=array(
 	<div class="row">
 		<?php echo $form->labelEx($model,'password'); ?>
 		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
+		<?php echo $form->error($model,'password'); ?>		
 	</div>
 
 	<div class="row rememberMe">
