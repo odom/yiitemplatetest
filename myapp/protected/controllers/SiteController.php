2359d13bcce9e796f91ebcc07f33842abf79f29d
SreyPov <sreypov.cheng@mapring.com>
2014-05-02 14:47:44 +0700

    [ADD] register_form.html

--- myapp/protected/controllers/SiteController.php
+++ myapp/protected/controllers/SiteController.php
@@ -106,4 +106,12 @@ class SiteController extends Controller
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
+        
+        /**
+	 * Register Action
+	 */
+	public function actionRegister()
+	{
+            $this->render('register');
+	}
 }
