22f47697c4d9427c46e36ab3c080b35a98dc8fc8
JOHN Odom <bidam.neo@gmail.com>
2014-05-02 02:36:19 -0700

    [TASK] 991 Signup Screen

--- myapp/protected/views/site/register.php
+++ myapp/protected/views/site/register.php
@@ -3,26 +3,23 @@
 	<div clas="row">
     	<div class="register  col-sm-offset-4 col-sm-4">
             <form role="form">
-            <h3 style="color:white">Create an Acount</h3>
-              <div class="form-group">
-                <input type="text" class="form-control" name="username" placeholder="Username" required="required">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control" name="con_password" placeholder="Retype Password" required="required">
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control" name="email" placeholder="Email(Optional)" required="required">
-              </div>
-              <div class="text-center">
-              	By acount condition to <a href="#" style="color:white">Terms & Condition of use</a>
-              </div>
-              <div class="form-group col-sm-offset-4">
-              <br/>
-              <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
+                <h3 style="color:white">Create an Acount</h3>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="con_password" placeholder="Retype Password" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" name="email" placeholder="Email(Optional)" required="required">
+                </div>
+                <div class="text-center">
+                    <p>By acount condition to <a href="#" style="color:white">Terms &amp; Condition of use</a></p>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
 	</div>
