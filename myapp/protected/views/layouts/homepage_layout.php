3c0e430a24e5b9689c2c76f47e9b30f9d762f331
SreyPov <sreypov.cheng@mapring.com>
2014-05-06 11:16:01 +0700

    [add] home page layout

--- myapp/protected/views/layouts/homepage_layout.php
+++ myapp/protected/views/layouts/homepage_layout.php
@@ -1,96 +1,106 @@
-
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+        <!-- blueprint CSS framework -->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+        <![endif]-->
+
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/homepage.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome.min.css" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/homepage.css" />
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
 
-<body class="homepage">
-    
-<div class="whole-content">
-  <div class="panel-left">
-      <div class="menu-left">
-          
-      </div>
-      <div class="nav-left"></div>
-  </div>
- <div class="main-content">
-     <?php //echo $content; ?>
- </div>
- <div class="panel-right">
-            <div class="nav-right"></div>
-            <div class="menu-right">
-                <div class="title-bar-right banner-md">
-                    <span class="triangle-top-right"></span>
-                    <i class="triangle-top-right-conner"></i>
-                    <span class="span-left">
-                        <i class="fa fa-sort-desc"></i> <span>Contact</span>
-                    </span>
-                    <span class="span-right">
-                        <a href="#"><i class="fa fa-plus-circle"></i></a>
-                        <a href="#"><i class="fa fa-minus-circle"></i></a>
-                    </span>
+    <body class="homepage">    
+        <div class="whole-content">
+            <div class="panel-left">
+                <div class="menu-left">
+                    <div class="menu-header banner-big">
+                        <span class="menu-header-left"></span>
+                        <i class="triangle-topleft"></i>
+                        <div class="menu-item">
+                            <span class="profile-image"><a href="#"><img src="../../images/avatar.png" /></a></span>
+                            <ul>
+                                <li><i class="fa fa-circle icon-online"></i></li>
+                                <li><i class="fa fa-info-circle icon-info"></i></li>
+                            </ul>
+                            <span class="profile-name">Sopheak</span>
+                        </div>
+                    </div>
+
+                    <div class="menu-header banner-md">
+                        <span class="menu-header-left"></span>
+                        <i class="triangle-topleft"></i>
+<!--                        <ul><i class="fa fa-sort-desc"></i></ul>-->
+                    </div>
+
+                    <div class="menu-header banner-md">
+                        <span class="menu-header-left"></span>
+                        <i class="triangle-topleft"></i>
+                    </div>                   
                 </div>
-                
-                <div class="contact-list">
-                    <ul>
-                          <li><a href="#Floophie">Floophie</a></li>
-                          <li><a href="#Bill">Bill</a></li>
-                    </ul>
-                </div>
-                
-               <div class="title-bar-right banner-md">
-                    <span class="triangle-top-right"></span>
-                    <i class="triangle-top-right-conner"></i>
-                    <span class="span-left">
-                        <i class="fa fa-sort-desc"></i> <span>Guests in Room</span>
-                    </span>
-                    <span class="span-right">
-                        <a href="#"><i class="fa fa-minus-circle"></i></a>
-                    </span>
-                </div>
-               <div class="user-online">
-                    <ul>
-                        <li>
-                            <div class="div-avatar">     
-                                <img src="../images/avatar.png"/>
-                            <i class="fa fa-circle fa-online"></i>
-                            <span>Floophie</span>
-                            <i class="fa fa-phone"></i>
-                            <i class="fa fa-eye"></i>
-                            <i class="fa fa-comment-o"></i>
-                            <i class="fa fa-info-circle"></i>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <div class="nav-left"></div>
             </div>
-            
-           
- </div>
+            <div class="main-content">
+                <?php //echo $content; ?>
+            </div>
+            <div class="panel-right">
+                <div class="nav-right"></div>
+                <div class="menu-right">
+                    <div class="title-bar-right banner-md">
+                        <span class="triangle-top-right"></span>
+                        <i class="triangle-top-right-conner"></i>
+                        <span class="span-left">
+                            <i class="fa fa-sort-desc"></i> <span>Contact</span>
+                        </span>
+                        <span class="span-right">
+                            <a href="#"><i class="fa fa-plus-circle"></i></a>
+                            <a href="#"><i class="fa fa-minus-circle"></i></a>
+                        </span>
+                    </div>
 
+                    <div class="contact-list">
+                        <ul>
+                            <li><a href="#Floophie">Floophie</a></li>
+                            <li><a href="#Bill">Bill</a></li>
+                        </ul>
+                    </div>
 
-     
-</div>
-            
-
-</body>
+                    <div class="title-bar-right banner-md">
+                        <span class="triangle-top-right"></span>
+                        <i class="triangle-top-right-conner"></i>
+                        <span class="span-left">
+                            <i class="fa fa-sort-desc"></i> <span>Guests in Room</span>
+                        </span>
+                        <span class="span-right">
+                            <a href="#"><i class="fa fa-minus-circle"></i></a>
+                        </span>
+                    </div>
+                    <div class="user-online">
+                        <ul>
+                            <li>
+                                <div class="div-avatar">     
+                                    <img src="../images/avatar.png"/>
+                                    <i class="fa fa-circle fa-online"></i>
+                                    <span>Floophie</span>
+                                    <i class=""></i>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>                    
+            </div>
+        </div>          
+    </body>
 </html>
-
