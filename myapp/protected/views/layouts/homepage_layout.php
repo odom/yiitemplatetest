78e494d606d85fadd0b0497812e69307324a51ef
SreyPov <sreypov.cheng@mapring.com>
2014-05-06 10:30:48 +0700

    [modify] home layout

--- myapp/protected/views/layouts/homepage_layout.php
+++ myapp/protected/views/layouts/homepage_layout.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -66,9 +66,16 @@
                         <a href="#"><i class="fa fa-minus-circle"></i></a>
                     </span>
                 </div>
-               <div class="contact-list">
+               <div class="user-online">
                     <ul>
-                          <li><a href="#Floophie">Floophie</a></li>
+                        <li>
+                            <div class="div-avatar">     
+                                <img src="../images/avatar.png"/>
+                            <i class="fa fa-circle fa-online"></i>
+                            <span>Floophie</span>
+                            <i class=""></i>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -83,43 +90,4 @@
 
 </body>
 </html>
-=======
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="en" />
 
-        <!-- blueprint CSS framework -->
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-        <!--[if lt IE 8]>
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-        <![endif]-->
-
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/homepage.css" />
-
-        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-    </head>
-
-    <body class="homepage">    
-        <div class="whole-content">
-            <div class="panel-left">
-                <div class="menu-left"></div>
-                <div class="nav-left"></div>
-            </div>
-            <div class="main-content">
-                <?php //echo $content; ?>
-            </div>
-            <div class="panel-right">
-                <div class="nav-right"></div>
-                <div class="menu-right"></div>
-            </div>
-        </div>          
-    </body>
-</html>
->>>>>>> 41ea84bcb3a254721406c089c6049e82e063a930
