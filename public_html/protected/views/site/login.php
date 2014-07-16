46edcd8693bef8b887dce07bb7a3ab7b35fe9f72
Neo Bidam <bidam.neo@gmail.com>
2014-07-16 14:55:22 +0700

    [CSS] Style on Button Login

--- public_html/protected/views/site/login.php
+++ public_html/protected/views/site/login.php
@@ -1,26 +1,32 @@
 <div class="loginform">
-
-			<form role="form" action="/user/login" method="post">
-				<div class="col-sm-10 col-sm-offset-1">
-				    <h3> Sign in</h3>
-				    <div class="text-center">
-				       <div class="input-group input-md">
-				          <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
-				          <input class="form-control input-lg" type="text" name="username" placeholder="User Name">
-				        </div>
-				        <br>
-				        <div class="input-group">
-				          <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
-				          <input class="form-control input-lg" type="password" name="password" placeholder="Password">
-				        </div>
-				        <br>
-				        <button type="submit"> <a class="btn btn-primary btn-lg"> &nbsp;&nbsp;&nbsp; ENTER &nbsp;&nbsp;&nbsp;</a></button>
-				        <div class="social-sign-in">
-				            <p><a class="btn btn-primary" href="#"><i class="fa fa-facebook"></i> &nbsp;Sign in with Facebook</a></p>
-				            <p><a class="btn btn-info" href="#"><i class="fa fa-twitter"></i>&nbsp;Sign in with Twitter</a></p>
-				            <p><a class="btn btn-danger" href="#"><i class="fa fa-google-plus"></i>&nbsp;Sign in with Google</a></p>
-				        </div>
-				    </div>
+	<form role="form" action="/user/login" method="post">
+		<div class="col-sm-10 col-sm-offset-1">
+			<h3> Sign in</h3>
+			<div class="text-center">
+				<div class="input-group input-md">
+					<span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+					<input class="form-control input-lg" type="text" name="username" placeholder="User Name">
 				</div>
-			</form>
+				<br>
+
+				<div class="input-group">
+					<span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
+					<input class="form-control input-lg" type="password" name="password" placeholder="Password">
+				</div>
+				<br>
+				<button type="submit" class="btn btn-lg btn-primary">&nbsp;&nbsp;&nbsp; ENTER &nbsp;&nbsp;&nbsp;</button>
+				<!--
+				<div class="social-sign-in">
+					<p><a class="btn btn-primary" href="#"><i class="fa fa-facebook"></i> &nbsp;Sign in with
+							Facebook</a></p>
+
+					<p><a class="btn btn-info" href="#"><i class="fa fa-twitter"></i>&nbsp;Sign in with Twitter</a></p>
+
+					<p><a class="btn btn-danger" href="#"><i class="fa fa-google-plus"></i>&nbsp;Sign in with Google</a>
+					</p>
+				</div>
+				-->
+			</div>
+		</div>
+	</form>
 </div>
