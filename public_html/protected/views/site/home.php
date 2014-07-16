046d1f9f52ddbe592726b26ea1042cc4cb9d851b
Neo Bidam <bidam.neo@gmail.com>
2014-07-17 06:51:05 +0700

    [TASK] avoid error in /site/home

--- public_html/protected/views/site/home.php
+++ public_html/protected/views/site/home.php
@@ -1,1 +1,8 @@
+
+<?php
+/**
+ * TODO: Fix the $output to get the right content.
+ */
+if (!isset($output)) $output= array('object'=>'none');
+?>
 <textarea id="userData" class="hidden"><?php  echo json_encode($output); ?></textarea>
