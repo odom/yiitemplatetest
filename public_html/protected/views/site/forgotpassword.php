89e9b45c3c8f37ebe2fcd2ec35b40feecece486f
Sokang Soy <sokangsoy1991@gmail.com>
2014-07-24 11:26:15 +0700

    [add] color box

--- public_html/protected/views/site/forgotpassword.php
+++ public_html/protected/views/site/forgotpassword.php
@@ -1,34 +1,41 @@
-<form class="form-horizontal">
-	<fieldset>
 
-		<!-- Form Name -->
-		<legend>Form Name</legend>
+<div id="forgot">
 
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="username">Username:</label>
-			<div class="col-md-4">
-				<input id="username" name="username" placeholder="Username" class="form-control input-md" type="text">
+	<form class="form-horizontal">
+		<fieldset>
 
+			<!-- Form Name -->
+			<legend>Reset Password</legend>
+
+			<!-- Text input-->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="username">Username:</label>
+				<div class="col-md-4">
+					<input id="username" name="username" placeholder="Username" class="form-control input-md" type="text">
+
+				</div>
 			</div>
-		</div>
 
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="email">Email:</label>
-			<div class="col-md-4">
-				<input id="email" name="email" placeholder="Alternative email" class="form-control input-md" type="email">
+			<!-- Text input-->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="email">Email:</label>
+				<div class="col-md-4">
+					<input id="email" name="email" placeholder="Alternative email" class="form-control input-md" type="email">
 
+				</div>
 			</div>
-		</div>
 
-		<!-- Button -->
-		<div class="form-group">
-			<label class="col-md-4 control-label" for="reset"></label>
-			<div class="col-md-4">
-				<button id="reset" name="reset" class="btn btn-primary">Reset</button>
+			<!-- Button -->
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="reset"></label>
+				<div class="col-md-4">
+					<button id="reset" name="reset" class="btn btn-primary">Reset</button>
+				</div>
 			</div>
-		</div>
 
-	</fieldset>
-</form>
+		</fieldset>
+	</form>
+
+
+</div>
+
